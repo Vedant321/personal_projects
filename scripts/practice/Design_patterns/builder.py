1313a4fb3863dc5be162dc4069b82c446120152e
Vedant Shinde <vedantve@gmail.com>
2025-01-05 20:06:55 +0530

    BRANCH-1: First two creational patterns

---
+++ scripts/practice/Design_patterns/builder.py
@@ -0,0 +1,36 @@
+class Burger:
+    def __init__(self):
+        self.buns = None
+        self.patty = None
+        self.cheese = None
+
+    def setBuns(self, buntStyle):
+        self.buns = buntStyle
+
+    def setPatty(self, pattyStyle):
+        self.patty = pattyStyle
+    
+    def setCheese(self, cheeseStyle):
+        self.cheese = cheeseStyle
+
+class BurgerBuilder:
+    def __init__(self):
+        self.burger = Burger()
+
+    def addBuns(self, bunStyle):
+        self.burger.setBuns(bunStyle)
+        return self
+    
+    def addPatty(self, pattyStyle):
+        self.burger.setPatty(pattyStyle)
+        return self
+    
+    def addCheese(self, cheeseStyle):
+        self.burger.setCheese(cheeseStyle)
+        return self
+    
+burger = BurgerBuilder()\
+        .addBuns("sesame")\
+        .addCheese("Swiss-cheese")\
+        .addPatty("fish-patty")
+
